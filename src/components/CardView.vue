58cf7280863c88b93cdde621cf8c6774afcdb3b1
zaki bousmaha <zakib9@gmail.com>
2024-08-12 00:19:38 +0100

    template
    start adding component to the page and modifying json file

---
+++ src/components/CardView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="flex justify-center relative">
+            <img class="object-cover h-80 w-80 rounded-lg border-solid border-2
+            border-transparent " 
+            :src= "`./src/assets/images/${card.image.desktop}`">
+            
+            <!-- <img class="object-cover h-80 w-80 rounded-lg border-solid border-2
+            border-transparent hover:border-red-500" 
+            src="../assets/images/image-waffle-desktop.jpg"> -->
+            <AddToCartView/>
+        </div>
+       
+        <div class="my-8">
+            <span class="text-gray-500 text-sm font-medium ">{{card.category}}</span>
+            <h4 class="text-md font-bold">{{card.name}}</h4>
+            <h3 class="text-primary text-lg font-RedHatBold">${{card.price}}</h3>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import AddToCartView from './AddToCartView.vue';
+
+
+defineProps({
+  card: {
+    type: Object,
+    default: () => {},
+  },
+});
+//console.log(card)
+</script>
+
