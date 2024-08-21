6b9d2ee61c437dfe937e6de72882a8c9906c5c5b
zaki bousmaha <zakib9@gmail.com>
2024-08-21 14:02:44 +0100

    Update CardView.vue

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -3,10 +3,10 @@
         <div  class="flex justify-center relative">
             <img v-if="card.isActive" class="object-cover h-80 w-80 rounded-lg border-solid border-2
             border-primary"
-             :src= "`./src/assets/images/${card.image.desktop}`">
+            :src= "`./src/assets/images/${card.image.desktop}`">
             <img v-else class="object-cover h-80 w-80 rounded-lg border-solid border-2
             border-transparent"
-             :src= "`./src/assets/images/${card.image.desktop}`">
+            :src= "`./src/assets/images/${card.image.desktop}`">
            <AddToCartView  :card = "card" @activation="onActivation" @decrementation="onDecrementation" @incrementation="onIncrementation"/>
         </div>
        
@@ -50,4 +50,3 @@ import AddToCartView from './AddToCartView.vue';
     });
 
 </script>
-
