d45ce9ef11cb89e2bd5fb25c4fc26286a168929c
zaki bousmaha <zakib9@gmail.com>
2024-08-21 14:22:22 +0100

    update

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -1,56 +1,52 @@
 <template>
-    <div>
-        <div  class="flex justify-center relative">
-            <img v-if="card.isActive" class="object-cover h-80 w-80 rounded-lg border-solid border-2
-            border-primary"
-            :src=baklavaImage />
-            <img v-else class="object-cover h-80 w-80 rounded-lg border-solid border-2
-            border-transparent"
-            src="../assets/images/image-baklava-desktop.jpg" />
-           <AddToCartView  :card = "card" @activation="onActivation" @decrementation="onDecrementation" @incrementation="onIncrementation"/>
-        </div>
-       
-        <div class="my-8">
-            <span class="text-gray-500 text-sm font-medium ">{{card.category}}</span>
-            <h4 class="text-md font-bold">{{card.name}}</h4>
-            <h3 class="text-primary text-lg font-RedHatBold">${{card.price}}</h3>
-        </div>
-    </div>
+  <div>
+      <div  class="flex justify-center relative">
+          <img v-if="card.isActive" class="object-cover h-80 w-80 rounded-lg border-solid border-2
+          border-primary"
+          :src= "`./src/assets/images/${card.image.desktop}`">
+          <img v-else class="object-cover h-80 w-80 rounded-lg border-solid border-2
+          border-transparent"
+          :src= "`./src/assets/images/${card.image.desktop}`">
+         <AddToCartView  :card = "card" @activation="onActivation" @decrementation="onDecrementation" @incrementation="onIncrementation"/>
+      </div>
+     
+      <div class="my-8">
+          <span class="text-gray-500 text-sm font-medium ">{{card.category}}</span>
+          <h4 class="text-md font-bold">{{card.name}}</h4>
+          <h3 class="text-primary text-lg font-RedHatBold">${{card.price}}</h3>
+      </div>
+  </div>
 </template>
 
 <script setup>
 
 import AddToCartView from './AddToCartView.vue';
-import baklavaImage from "../assets/images/image-macaron-desktop.jpg"
-
- 
- 
 
 
-    const emit = defineEmits([
-          "activation",
-          "incrementation",
-          "decrementation"
-        ])
-    // functionality emitation to home vue
-    function onActivation(){
-      emit("activation")
-    }
-    function onIncrementation(){
-      emit("incrementation")
-    }
-    function onDecrementation(){
-      emit("decrementation")
-     
-    }
+  const emit = defineEmits([
+        "activation",
+        "incrementation",
+        "decrementation"
+      ])
+  // functionality emitation to home vue
+  function onActivation(){
+    emit("activation")
+  }
+  function onIncrementation(){
+    emit("incrementation")
+  }
+  function onDecrementation(){
+    emit("decrementation")
    
+  }
+ 
 
-    defineProps({
-      card: {
-        type: Object,
-        default: () => {},
-      },
-     
-    });
+  defineProps({
+    card: {
+      type: Object,
+      default: () => {},
+    },
+   
+  });
 
 </script>
