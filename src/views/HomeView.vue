958b65d48136491b7c522e23ab5b30e5e1be9799
zaki bousmaha <zakib9@gmail.com>
2024-09-17 15:56:58 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,27 +6,7 @@ import cardsData from '../data.json'
 import { ref } from 'vue';
 
 
-import waffleImage from '@/assets/images/image-waffle-desktop.jpg'
-import vanillaImage from '@/assets/images/image-creme-brulee-desktop.jpg'
-import macaronImage from '@/assets/images/image-macaron-desktop.jpg'
-import tiramisuImage from '@/assets/images/image-tiramisu-desktop.jpg'
-import baklavaImage from '@/assets/images/image-baklava-desktop.jpg'
-import meringueImage from '@/assets/images/image-meringue-desktop.jpg'
-import velvetImage from '@/assets/images/image-cake-desktop.jpg'
-import brownieImage from '@/assets/images/image-brownie-desktop.jpg'
-import pannaImage from '@/assets/images/image-panna-cotta-desktop.jpg'
 
-const images = ref([
-{ src: waffleImage },
-{ src: vanillaImage },
-{ src: macaronImage },
-{ src: tiramisuImage },
-{ src: baklavaImage },
-{ src: meringueImage },
-{ src: velvetImage },
-{ src: brownieImage },
-{ src: pannaImage },
-])
 
 
 const items = ref([])
@@ -39,7 +19,7 @@ let confirmationOrderPopUp = ref(null)
   function cardsDataModification(cards){
     for (let index = 0; index < cards.length; index++) {
       items.value.push({
-            "image": images.value[index].src,
+            "image": cards[index].image,
             "name": cards[index].name,
             "category": cards[index].category,
             "price": cards[index].price,
