585597a425af949bb9ec8877186d3763afd3c2b7
zaki bousmaha <zakib9@gmail.com>
2024-09-17 15:21:33 +0100

    required

--- src/components/CardView.vue
+++ src/components/CardView.vue
@@ -3,10 +3,10 @@
       <div class="flex justify-center relative">
               <img v-if="card.isActive" class="object-cover h-80 w-80 rounded-lg border-solid border-2
               border-primary"
-              :src= "card.image">
+              :src= "require(card.image)">
               <img v-else class="object-cover h-80 w-80 rounded-lg border-solid border-2
               border-transparent"
-              :src= "card.image">
+              :src= "require(card.image)">
                        <AddToCartView  :card = "card" @activation="onActivation" @decrementation="onDecrementation" @incrementation="onIncrementation"/>
 
       </div>
